07d0c3f794f6b919154ac759aa5b6872a95927d8
JohnSkull22 <nischintasharma22@gmail.com>
2024-07-10 17:33:27 +0530

    c# file commit

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public enum DaysOfWeek
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+class Program
+{
+    static void Main()
+    {
+        DaysOfWeek today = DaysOfWeek.Wednesday;
+        Console.WriteLine($"Today is {today}");
+    }
+}
